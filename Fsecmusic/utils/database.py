842e919da133698f19f74a944564b0c1790ab009
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-19 23:33:21 -0700

    Update database.py

--- Fsecmusic/utils/database.py
+++ Fsecmusic/utils/database.py
@@ -468,13 +468,11 @@ async def is_served_user(user_id: int) -> bool:
         return False
     return True
 
-
 async def get_served_users() -> list:
     users_list = []
     async for user in usersdb.find({"user_id": {"$gt": 0}}):
         users_list.append(user)
     return users_list
-
 
 async def add_served_user(user_id: int):
     is_served = await is_served_user(user_id)
@@ -482,6 +480,8 @@ async def add_served_user(user_id: int):
         return
     return await usersdb.insert_one({"user_id": user_id})
 
+async def remove_served_user(user_id: int):
+    return await usersdb.delete_one({"user_id": user_id})
 
 async def get_served_chats() -> list:
     chats_list = []
@@ -489,19 +489,21 @@ async def get_served_chats() -> list:
         chats_list.append(chat)
     return chats_list
 
-
 async def is_served_chat(chat_id: int) -> bool:
     chat = await chatsdb.find_one({"chat_id": chat_id})
     if not chat:
         return False
     return True
 
-
 async def add_served_chat(chat_id: int):
     is_served = await is_served_chat(chat_id)
     if is_served:
         return
     return await chatsdb.insert_one({"chat_id": chat_id})
+
+async def remove_served_chat(chat_id: int):
+    return await chatsdb.delete_one({"chat_id": chat_id})
+
 
 
 async def blacklisted_chats() -> list:
