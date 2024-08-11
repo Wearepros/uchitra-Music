ed7dec083ae5b768d37be99733898a2da8d706a0
UNEMPLOID <141543574+UNEMPLOID@users.noreply.github.com>
2024-08-11 13:32:52 +0530

    Update callback.py

--- Fsecmusic/plugins/admins/callback.py
+++ Fsecmusic/plugins/admins/callback.py
@@ -11,7 +11,7 @@ from Fsecmusic.utils.database import get_assistant
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from Fsecmusic import YouTube, app, YTB
-from Fsecmusic.core.call import JARVIS
+from Fsecmusic.core.call import FALCON
 from Fsecmusic.misc import SUDOERS, db
 from Fsecmusic.utils.database import (
     get_active_chats,
@@ -256,7 +256,7 @@ async def del_back_playlist(client, CallbackQuery, _):
         )
     elif command == "Stop" or command == "End":
         await CallbackQuery.answer()
-        await JARVIS.stop_stream(chat_id)
+        await FALCON.stop_stream(chat_id)
         await set_loop(chat_id, 0)
         await CallbackQuery.message.reply_text(
             _["admin_5"].format(mention), reply_markup=close_markup(_)
@@ -269,7 +269,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             )
         await CallbackQuery.answer()
         await mute_on(chat_id)
-        await JARVIS.mute_stream(chat_id)
+        await FALCON.mute_stream(chat_id)
         await CallbackQuery.message.reply_text(
             _["admin_46"].format(mention)
         )
@@ -280,7 +280,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             )
         await CallbackQuery.answer()
         await mute_off(chat_id)
-        await JARVIS.unmute_stream(chat_id)
+        await FALCON.unmute_stream(chat_id)
         await CallbackQuery.message.reply_text(
             _["admin_48"].format(mention)
         )
@@ -348,7 +348,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                         ),
                         reply_markup=close_markup(_),
                     )
-                    return await JARVIS.stop_stream(chat_id)
+                    return await FALCON.stop_stream(chat_id)
                 except:
                     return
         else:
@@ -380,7 +380,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             except:
                 image = None
             try:
-                await JARVIS.skip_stream(chat_id, link, video=status, image=image)
+                await FALCON.skip_stream(chat_id, link, video=status, image=image)
             except:
                 return await CallbackQuery.message.reply_text(_["call_6"])
             button = stream_markup(_, chat_id)
@@ -424,7 +424,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             except:
                 image = None
             try:
-                await JARVIS.skip_stream(chat_id, file_path, video=status, image=image)
+                await FALCON.skip_stream(chat_id, file_path, video=status, image=image)
             except:
                 return await mystic.edit_text(_["call_6"])
             button = stream_markup(_, chat_id)
@@ -445,7 +445,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             await mystic.delete()
         elif "index_" in queued:
             try:
-                await JARVIS.skip_stream(chat_id, videoid, video=status)
+                await FALCON.skip_stream(chat_id, videoid, video=status)
             except:
                 return await CallbackQuery.message.reply_text(_["call_6"])
             button = stream_markup(_, chat_id)
@@ -564,7 +564,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             if n == 0:
                 return await mystic.edit_text(_["admin_22"])
         try:
-            await JARVIS.seek_stream(
+            await FALCON.seek_stream(
                 chat_id,
                 file_path,
                 seconds_to_min(to_seek),
