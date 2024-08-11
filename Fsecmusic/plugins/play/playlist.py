fab1cd160bfc9e5b94df22f603669d3a1857b648
UNEMPLOID <141543574+UNEMPLOID@users.noreply.github.com>
2024-08-11 14:50:53 +0530

    Update playlist.py

--- Fsecmusic/plugins/play/playlist.py
+++ Fsecmusic/plugins/play/playlist.py
@@ -115,7 +115,7 @@ async def check_playlist(client, message: Message, _):
         count += 1
         msg += f"\n\n{count}- {title[:70]}\n"
         msg += _["playlist_5"].format(duration)
-    link = await ANNIEBIN(msg)
+    link = await FSECBIN(msg)
     lines = msg.count("\n")
     if lines >= 17:
         car = os.linesep.join(msg.split(os.linesep)[:17])
