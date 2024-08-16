02969fabbf2afe87bf22babcee7733bc05b4193d
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-08-16 17:10:20 +0530

    Update bin.py

--- Fsecmusic/plugins/tools/bin.py
+++ Fsecmusic/plugins/tools/bin.py
@@ -3,7 +3,7 @@ from pyrogram import *
 from pyrogram.types import *
 
 
-@app.on_message(filters.command(["bin", "ccbin", "bininfo"], [".", "!", "/"]))
+@app.on_message(filters.command(["bin", "bininfo"], [".", "!", "/"]))
 async def check_ccbin(client, message):
     if len(message.command) < 2:
         return await message.reply_text(
